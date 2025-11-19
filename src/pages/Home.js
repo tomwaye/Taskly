1ba09efea54362bf5dc49a9a0d845fc6895836e8
schizo <schizo@debian.debian>
2025-11-19 01:49:44 +0000

    Inital Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+
+
+function Home(){
+    return(
+        <div className="h-screen flex items-top justify-center bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-500">Tailwind is working!</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // <-- makes Tailwind scan all components
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
